d6c12a7c4603617c07f49431d16ed9cd27a50bba
6stranik6 <6stranik6@mail.ru>
2024-02-22 18:06:17 +0300

    Made

--- Django/myproject/projec1/myapp/models.py
+++ Django/myproject/projec1/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Human(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    occupation = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
